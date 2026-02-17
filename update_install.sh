3323e24e5036c003792aa24871ea427be58fca3f
mirror222-222 <59978580+mirror222-222@users.noreply.github.com>
2026-02-17 19:15:56 +0000

    Simplify update_install.sh by removing GitHub authentication prompts and using default cloning method

--- update_install.sh
+++ update_install.sh
@@ -3,35 +3,17 @@
 
 REPO_URL="https://github.com/mirror222-222/minecraft-manager.git"
 
-# For private repos, set GITHUB_TOKEN or use username/password
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "GITHUB_TOKEN not set. If this is a private repo, you can use your GitHub username and password (or personal access token as password)."
-    read -p "GitHub Username: " GITHUB_USER
-    read -s -p "GitHub Password or Token: " GITHUB_PASS
-    echo
-fi
+TARGET_DIR="/opt/minecraft"
 TARGET_DIR="/opt/minecraft"
 
 
 if [ -d "$TARGET_DIR/.git" ]; then
     echo "Updating existing repository in $TARGET_DIR..."
     cd "$TARGET_DIR" || exit 1
-    if [ -n "$GITHUB_TOKEN" ]; then
-        git pull "https://$GITHUB_TOKEN@${REPO_URL#https://}"
-    elif [ -n "$GITHUB_USER" ] && [ -n "$GITHUB_PASS" ]; then
-        git pull "https://$GITHUB_USER:$GITHUB_PASS@${REPO_URL#https://}"
-    else
-        git pull origin main
-    fi
+    git pull origin main
 else
     echo "Cloning repository into $TARGET_DIR..."
-    if [ -n "$GITHUB_TOKEN" ]; then
-        git clone "https://$GITHUB_TOKEN@${REPO_URL#https://}" "$TARGET_DIR"
-    elif [ -n "$GITHUB_USER" ] && [ -n "$GITHUB_PASS" ]; then
-        git clone "https://$GITHUB_USER:$GITHUB_PASS@${REPO_URL#https://}" "$TARGET_DIR"
-    else
-        git clone "$REPO_URL" "$TARGET_DIR"
-    fi
+    git clone "$REPO_URL" "$TARGET_DIR"
 fi
 
 echo "Installing Python dependencies..."
